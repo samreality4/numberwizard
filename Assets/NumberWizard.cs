42dd8f2c2b64d7902692f5bc73fc4ba6174265bd
sam <samreality4@gmail.com>
2020-06-24 17:40:47 -0700

    if else statements

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -4,17 +4,50 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
+      int max = 1000;
+        int min = 1;
+        int guess = 500;
     // Start is called before the first frame update
     void Start()
     {
+      
+
         Debug.Log("Welcome to number wizard!");
-        print("Welcome!");
-        
+        Debug.Log("Pick a number, don't tell me what it is...");
+        Debug.Log("The highest number  you can pick is:" + max);
+        Debug.Log("The lowest number you can pick is:" + min);
+        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Detect when the up arrow key is pressed down
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed.");
+            min=guess;
+            Debug.Log(guess);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down Arrow key was pressed.");
+            max=guess;
+            Debug.Log(guess);
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Enter key was pressed.");
+        }
+
+
+
+
+
+
+
     }
 }
